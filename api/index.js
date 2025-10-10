23955050bdb53e01260d99022c0a66e27d3231a8
unknown <bintang.arimedika@gmail.com>
2025-10-10 22:50:24 +0700

    update indexxjs

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// Pastikan path ini sesuai dengan struktur folder Anda
 import { 
   loginUser, 
   getAllData, 
@@ -15,30 +14,16 @@ import {
 dotenv.config();
 const app = express();
 
-// Konfigurasi CORS untuk production dan development
-const whitelist = [
-  'http://localhost:3000',
-  'https://satyadatafrontend-ot6ezmqji-bintangaris-projects.vercel.app' 
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
-// --- REVISI: SEMUA RUTE DIBERIKAN KEMBALI PREFIX /api ---
-app.post("/api/login", loginUser);
-app.get("/api/lokasi", getLokasi);
-app.get("/api/komoditas", getKomoditas);
-app.get("/api/data", getAllData);
-app.post("/api/data", postInputData);
-app.put("/api/data/:id", updateData);
-app.delete("/api/data/:id", deleteData);
+// --- REVISI FINAL: SEMUA PREFIX /api DIHAPUS DARI SINI ---
+app.post("/login", loginUser);
+app.get("/lokasi", getLokasi);
+app.get("/komoditas", getKomoditas);
+app.get("/data", getAllData);
+app.post("/data", postInputData);
+app.put("/data/:id", updateData);
+app.delete("/data/:id", deleteData);
 
 export default app;
