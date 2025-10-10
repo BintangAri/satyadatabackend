d89b387729ef3018230c704d699cbfd3544a7173
unknown <bintang.arimedika@gmail.com>
2025-10-10 22:36:45 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
-// File: /api/index.js
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -19,14 +17,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// --- PASTIKAN SEMUA RUTE INI ADA ---
-app.post("/api/login", loginUser);
-app.get("/api/lokasi", getLokasi);
-app.get("/api/komoditas", getKomoditas);
-app.get("/api/data", getAllData);
-app.post("/api/data", postInputData);
-app.put("/api/data/:id", updateData);
-app.delete("/api/data/:id", deleteData);
+// --- REVISI: SEMUA PREFIX /api DIHAPUS DARI SINI ---
+app.post("/login", loginUser);
+app.get("/lokasi", getLokasi);
+app.get("/komoditas", getKomoditas);
+app.get("/data", getAllData);
+app.post("/data", postInputData);
+app.put("/data/:id", updateData);
+app.delete("/data/:id", deleteData);
 
-// Export app untuk Vercel
 export default app;
