8578afd047804aa12e0c9ecea4c09ab17f3378bf
unknown <bintang.arimedika@gmail.com>
2025-10-10 22:42:58 +0700

    update indexx

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
-// File: /api/index.js
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+// Pastikan path ini sesuai dengan struktur folder Anda
 import { 
   loginUser, 
   getAllData, 
@@ -16,10 +15,24 @@ import {
 dotenv.config();
 const app = express();
 
-app.use(cors());
+// Konfigurasi CORS untuk production dan development
+const whitelist = [
+  'http://localhost:3000',
+  'https://satyadatafrontend-ot6ezmqji-bintangaris-projects.vercel.app' 
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
-// --- PASTIKAN SEMUA RUTE INI ADA ---
+// --- REVISI: SEMUA RUTE DIBERIKAN KEMBALI PREFIX /api ---
 app.post("/api/login", loginUser);
 app.get("/api/lokasi", getLokasi);
 app.get("/api/komoditas", getKomoditas);
@@ -28,5 +41,4 @@ app.post("/api/data", postInputData);
 app.put("/api/data/:id", updateData);
 app.delete("/api/data/:id", deleteData);
 
-// Export app untuk Vercel
 export default app;
